c76c713e712463925b31f580652165ecad1dac6d
2366567031 <87292673+2366567031@users.noreply.github.com>
2021-07-30 11:23:47 +0800

    Add files via upload

--- 作业.py
+++ 作业.py
@@ -1,37 +1,183 @@
-import  xlrd
-import  pymysql
-import  xlwt
-from DBUtils import*
-def pm():
-    wd = xlrd.open_workbook("12月份衣服销售数据.xlsx", encoding_override=True)
-    # 2.打开您要操作的选项卡
-    st = wd.sheet_by_name("12月份各种服饰销售情况")
-    rows = st.nrows  # 获取行数
-    cols = st.ncols  # 获取列数
-    for i in range(1,rows):
-        data = st.row_values(i)
-        sql = "insert into tea values(%s,%s,%s,%s,%s)"
-        update(sql,data)
-pm()
-def pt():
-    tea = ["日期","服装名称","价格/件","库存数量","销售量/日"]
-    #获取数据库中所有数据
-    sql = "select * from tea"
-    param = []
-    data = select(sql,param,mode="all",size=1)
-    print(data)
-    rows = len(data)
-    cols = len(data[0])
-    workbook = xlwt.Workbook(encoding='utf-8')
-    sheet1 = workbook.add_sheet("12衣服销售情况")
-    for i in range(rows+1):
-        for j in range(cols):
+# class Shuibei:
+#     __name = ""
+#     __gaodu = 0
+#     __rongji = 0
+#     __yanse = ""
+#     __caizhi = ""
+#
+#     def setname(self,name):
+#         self.__name = name
+#
+#     def getname(self):
+#         return self.__name
+#
+#     def setgaodu(self,gaodu):
+#         if gaodu > 20 :
+#             print("高度过高，请重新输入！")
+#         else :
+#             self.__gaodu = gaodu
+#
+#     # def getgaodu(self):
+#     #     return self.__gaodu
+#
+#     def setrongji(self,rongji):
+#         if rongji > 20 :
+#             print("容积过大，请重新输入！")
+#         else:
+#             self.__rongji = rongji
+#
+#     def rongji(self,rongji):
+#         print(self.__name,"的容积为",rongji)
+#
+#     # def getrongji(self):
+#     #     return self.__rongji
+#
+#     def setyanse(self,yanse):
+#         self.__yanse = yanse
+#
+#     # def getyanse(self):
+#     #     return self.__yanse
+#
+#     def setcaizhi(self,caizhi):
+#         self.__caizhi = caizhi
+#
+#     # def getcaizhi(self):
+#     #     return self.__caizhi
+#
+#     def gaodu(self,gaodu):
+#         print("水杯的高度为",gaodu)
+#
+#
+#     def beizi(self):
+#         print("水杯的高度为：",self.__gaodu,",容积为：",self.__rongji,"水杯的颜色是，",self.__caizhi,"水杯的材质是，",self.__caizhi)
+#
+#     def zuoyong(self):
+#         print("这个水杯可以存放",self.__rongji,"ml","的液体")
+#
+# s=Shuibei()
+# s.setname ("富广")
+# #水杯的高度
+#
+# s.setgaodu  (10)
+# #水杯的容积
+# s.setrongji (10)
+# #水杯的颜色
+# s.setyanse("红色")
+# #水杯的材质
+# s.setcaizhi("塑料")
+#
+#
+# print(s.getname())
+#
 
-            if i == 0:
-                sheet1.write(i, j, tea[j])
-            else:
-                sheet1.write(i,j,data[i-1][j])
-    workbook.save("D:\workspace\pythonProject\day9\dame.xls")
-    print("创建excel成功：")
 
-pt()
+
+class shuibei:
+    __name = ""
+    __rongji = 0
+    __gaodu =  0
+
+    def setname(self,name):
+        self.__name = name
+
+    def getname(self):
+        return self.__name
+
+    def setrongji(self,rongji):
+        if rongji >= 20 :
+            print("容积过大，重新输入！")
+        else:
+            self.__rongji = rongji
+
+    def setgaodu(self,gaodu):
+        self.__gaodu =   gaodu
+
+    def weizhi(self,weizhi):
+        print(self.__name,"放在",weizhi)
+
+    def zhuren(self,zhuren):
+        print(self.__name,"的主人是",zhuren)
+
+s=shuibei()
+
+s.setname("富广水杯")
+s.setgaodu(20)
+s.setrongji(10)
+
+s.weizhi("桌子上")
+s.zhuren("车志阳")
+
+
+
+
+class diannao :
+    __pingmu = 0
+    __jiage = 0
+    __cup = ""
+    __neicun = 0
+    __daiji = 0
+
+    def setpingmu(self,pingmu):
+        self.__pingmu = pingmu
+
+    def getpingmu(self):
+        return self.__pingmu
+
+    def setjiage(self,jiage):
+        self.__jiage = jiage
+
+    def getjiage(self):
+        return self.__jiage
+
+    def setcpu(self,cpu):
+        self.__cpu = cpu
+
+    def getcpu(self):
+        return self.__cpu
+
+    def setneicun(self,neicun):
+        self.__neicun = neicun
+
+    def getneicun(self):
+        return self.__neicun
+
+    def setdaiji(self,daiji):
+        self.__daiji = daiji
+
+    def getdaiji(self):
+        return self.__daiji
+
+    def dazi(self,dazi):
+        print("这个电脑可以",dazi)
+
+    def youxi(self,youxi):
+        print("这个电脑可以玩",youxi)
+
+    def shipin(self,shipin):
+        print("这个电脑可以看",shipin)
+
+
+d=diannao()
+
+d.setpingmu(20)
+d.setjiage(5000)
+d.setcpu("因特")
+d.setneicun(16)
+d.setdaiji(20)
+
+d.dazi("打字")
+d.youxi("英雄联盟")
+d.shipin("小视频")
+
+
+
+
+
+
+
+
+
+
+
+
+
